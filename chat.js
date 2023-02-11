3507c82c60f24f1026346a8f63b195870e3d1b7b
qzmtch <40316715+qzmtch@users.noreply.github.com>
2023-02-11 18:23:12 +0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+ 
+http.createServer(function(request, response){
+     if(req.url === '/chat.m3u'){
+        fs.readFile("in.html", "utf8", function(error, data){
+             fetch('https://chaturbate.com/api/public/affiliates/onlinerooms/?wm=HNwJw&client_ip=172.70.54.110&limit=500&gender=f')
+            .then(response => response.json())
+            .then(rooms => buildM3U(rooms))
+            .then( m3u => {
+				data = data.replace("{m3u}", JSON.stringify(m3u));
+                        
+                response.end(data);
+			})
+			
+         
+        
+        
+        })
+	}
+    async function buildM3U(rooms) {
+      var keys = rooms['results'];
+      var logo="";
+      var m3u="#EXTM3U"+"\n";
+      for (var i = 0; i < keys.length; i++) {
+        var file = keys[i]; 
+        var link = 'https://edge10-sof.live.mmcdn.com/live-edge/amlst:'
+        var link2 = '/playlist.m3u8'
+        var logo=file['image_url'];
+        var name=file['username'];
+        
+            m3u+= '#EXTINF:-1 tvg-logo="'+logo+'"'+", "+name+"\n" + link + name + link2 + "\n";
+          
+        } 
+        return m3u
+      }
+}).listen(8080);
